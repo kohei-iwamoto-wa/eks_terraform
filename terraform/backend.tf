ceff2269ca3e2b29718bd7c34ecb6a36e090109c
kohei-iwamoto-wa <bananadesu9@gmail.com>
2026-05-06 12:44:44 +0900

    fix

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "path/to/my/key"
-    region         = "ap-northeast-1"
-    encrypt        = true
-  }
-}
