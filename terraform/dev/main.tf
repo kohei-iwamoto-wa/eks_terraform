e3480636dabd8422107ba806f13430b956529fb7
kohei-iwamoto-wa <bananadesu9@gmail.com>
2026-05-24 22:27:17 +0900

    bastion 追加

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -6,18 +6,20 @@ module "vpc" {
   source = "../modules/vpc"
 }
 
-# module "ec2_bastion" {
-#   source = "../modules/compute/bastion"
-#   subnet_id = module.vpc.private_subnets[0]
-#   security_group_ids = [module.eks.cluster_security_group_id]
-#   vpc_id = module.vpc.vpc_id
-#   vpc_cidr_block = module.vpc.vpc_cidr_block
-# }
+module "ec2_bastion" {
+  source = "../modules/compute/bastion"
+  subnet_id = module.vpc.private_subnets[0]
+  security_group_ids = []
+  vpc_id = module.vpc.vpc_id
+  vpc_cidr_block = module.vpc.vpc_cidr
+}
 
 module "eks" {
   source = "../modules/eks"
   vpc_id = module.vpc.vpc_id
+  security_group_ids = [module.ec2_bastion.security_group_id]
   cluster_subnet_ids = module.vpc.private_subnets
+  bastion_iam_role_arn = module.ec2_bastion.bastion_iam_role_arn
   cluster_name = "cluster"
   depends_on = [module.vpc]
 }
