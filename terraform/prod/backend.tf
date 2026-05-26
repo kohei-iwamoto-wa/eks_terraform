28c3a9756dca422a6903cc0f513d6c68b3eae553
kohei-iwamoto-wa <bananadesu9@gmail.com>
2026-05-26 23:53:14 +0900

    Remove backend.tf from tracking

--- terraform/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-730335414536-us-west-2"
-    key            = "prod/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    use_lockfile = true
-  }
-}
