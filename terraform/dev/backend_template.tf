da6fbe6b0f0f90ee65e141fec001de669cbe8641
kohei-iwamoto-wa <bananadesu9@gmail.com>
2026-06-07 17:12:59 +0900

    backendのテンプレート削除

--- terraform/dev/backend_template.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "バケット名"
-    key            = "stateファイルのパス"
-    region         = "リージョン"
-    encrypt        = true
-    use_lockfile = true
-  }
-}
